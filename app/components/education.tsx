8a695a763a17b818dbc3a2dfe82c12833422a861
luanamsilva <luanamoura2002@yahoo.com.br>
2023-10-14 08:19:02 -0300

    add-education

---
+++ app/components/education.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <section className="h-screen flex justify-center items-center">
+    <div className='w-full md:w-3/4'>
+      <h1>Formação</h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, nihil est vero velit voluptas accusantium aliquam dolores. Veritatis nihil earum quisquam? Eligendi ducimus hic voluptate vel expedita, optio beatae dolorem!
+    </div>
+    </section>
+  )
+}
+
+export default Education
