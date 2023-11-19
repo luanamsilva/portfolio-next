be421a6ccb351a3fc09cdef26afb2c79f20aa0e1
luanamsilva <luanamoura2002@yahoo.com.br>
2023-11-19 17:36:43 -0300

    education-content

--- app/components/education.tsx
+++ app/components/education.tsx
@@ -5,7 +5,15 @@ const Education = () => {
     <section className="h-screen flex justify-center items-center">
     <div className='w-full md:w-3/4'>
     <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-pink-500 lg:text-5xl w-full pb-8'>Formação</h1>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, nihil est vero velit voluptas accusantium aliquam dolores. Veritatis nihil earum quisquam? Eligendi ducimus hic voluptate vel expedita, optio beatae dolorem!
+    <div className='p-3'> 
+      <h2 className='text-lg pb-2'>- Ciência da Computação: 2022-2025</h2>
+<p className='text-gray-700'>UNIVERSIA - Centro Universitário União das Américas Descomplica</p> 
+</div>
+  <div className='p-2'>
+    <h2 className='text-lg pb-2'>- Bacharelado em Administração: 2005 – 2009</h2>
+<p className='text-gray-700'>PUC MINAS – Pontifícia Universidade Católica de Minas Gerais</p>
+</div>
+
     </div>
     </section>
   )
